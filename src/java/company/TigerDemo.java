ea4e0f17dc42d79db1008c2e5f8c0951771f6d81
DoloresDai <841856753@qq.com>
2019-02-22 04:51:24 +0800

    Re-subcontracting folders.

--- src/java/company/TigerDemo.java
+++ src/java/company/TigerDemo.java
@@ -5,7 +5,7 @@ import main.java.company.model.Tiger;
 
 public class TigerDemo {
     public static void main(String[] args) {
-        Tiger tiger = new Tiger(2,20.2);
+    Tiger tiger  = new Tiger(2, 20.2);
         tiger.basicAttributes(tiger);
         tiger.meow();
     }
